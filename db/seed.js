0e4e414f96ba94bb00e9d3c54a9c24459a1a1840
Jordan <jordanmittelman@gmail.com>
2019-06-25 10:20:36 -0400

    seed file

---
+++ db/seed.js
@@ -0,0 +1,29 @@
+const Game = require("./models/Game.js");
+// const Event = require("./models/Event.js");
+const User = require("./models/User.js");
+
+const gamesData = require("./data/gameData.json");
+// const eventsData = require("./data/eventData.json");
+const usersData = require("./data/userData.json");
+
+Game.deleteMany({})
+  .then(() => {
+    Game.create(gamesData).then(gameDocs => {
+      console.log(gameDocs);
+      process.exit();
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+User.deleteMany({})
+  .then(() => {
+    User.create(usersData).then(userDocs => {
+      console.log(userDocs);
+      process.exit();
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
